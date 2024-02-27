31d9a4027285ebaab3b8b4ab00ddd7867358bd02
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2024-02-26 20:04:52 -0500

    do stuff

--- app1/src/import-resolver.ts
+++ app1/src/import-resolver.ts
@@ -8,93 +8,19 @@ const extensionPriority = {
   cjs: ['.cts', '.cjs'],
 };
 
-function loadRelativePath(
-  path: string,
-  importer: string,
-  getFile: (fileName: string) => FsFile | null
-) {}
-
-function UintArrayToString(arr: Uint8Array | string) {
-  if (typeof arr === 'string') {
-    return arr;
+function isURL(urlString: string) {
+  try {
+    return Boolean(new URL(urlString));
+  } catch (e) {
+    return false;
   }
-
-  let str = '';
-  for (let i = 0; i < arr.length; i++) {
-    str += String.fromCharCode(arr[i]);
-  }
-  return str;
 }
 
-interface ResolveResult {
-  path: string;
-  loader: string;
+function ESM_RESOLVE(specifier: string, parentURL: string) {
+  let resolved = undefined;
+
+  if (isURL(specifier)) {
+  }
 }
 
-function resolveFromImports(
-  source: string,
-  importer: string,
-  getFile: (fileName: string) => FsFile | null
-) {
-  // Go through the parent folders to find a package.json
-  let currentDir = resolve('/', importer);
-  let file: FsFile | null = null;
-  while (currentDir !== '/') {
-    file = getFile(resolve(currentDir, 'package.json'));
-    if (file) {
-      break;
-    }
-
-    currentDir = dirname(currentDir);
-  }
-
-  if (!file) {
-    throw new Error(
-      `Could not resolve import '${source}' from '${importer}': no package.json found`
-    );
-  }
-
-  const packageJson = JSON.parse(UintArrayToString(file.contents));
-
-  if (!packageJson.imports) {
-    throw new Error(
-      `Could not resolve import '${source}' from '${importer}', imports not defined in ${resolve(
-        currentDir,
-        'package.json'
-      )}`
-    );
-  }
-
-  if (!packageJson.imports[source]) {
-    throw new Error(
-      `Could not resolve import '${source}' from '${importer}', import not found`
-    );
-  }
-
-  return resolve(currentDir, packageJson.imports[source]);
-}
-
-function resolveImport(
-  source: string,
-  importer: string,
-  getFile: (fileName: string) => FsFile | null,
-  fileExists: (fileName: string) => boolean,
-  directoryExists: (directoryName: string) => boolean
-) {
-  if (source.startsWith('node:')) {
-    return source;
-  }
-
-  if (
-    source.startsWith('./') ||
-    source.startsWith('../') ||
-    source.startsWith('/')
-  ) {
-    return resolve(importer, source);
-  }
-
-  if (source.startsWith('#'))
-    return resolveFromImports(source, importer, getFile);
-}
-
-export default resolveImport;
+export default ESM_RESOLVE;
